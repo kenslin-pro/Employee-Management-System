b76afa16e1e6bfeb62f30bb95221ad536d3364c2
kenslin-pro <kenslinerohine@gmail.com>
2023-10-13 22:26:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Overview
+
+The text-based adventure game I developed serves as a learning tool for understanding and applying C# programming concepts. It presents players with a series of narrative choices, and their decisions dictate the direction of the story. The game's code is structured to handle user input, manage game states, and display text-based outcomes based on user decisions.
+
+The primary purpose of creating this software was to deepen my knowledge of the C# programming language and reinforce my understanding of fundamental concepts. By building a text-based game, I aimed to gain practical experience in working with conditional statements, loops, data structures, and user input handling. The project allowed me to apply these concepts in a fun and engaging way, enhancing my problem-solving skills and programming proficiency.
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I created a text-based adventure game using the C# programming language, which allowed me to demonstrate my skills and understanding of C# syntax and principles. To build and test the software, I used Visual Studio, a robust integrated development environment (IDE) for C# development. This IDE provided a wide range of tools, including a code editor, debugger, and project management features.
+# Useful Websites
+
+
+
+- [W3 School](https://www.w3schools.com/cs/cs_while_loop.phps)
+- [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+
+
+- Enhance Game Content: Expanding the storyline and adding more narrative branches to create a richer gameplay experience.
+- Multiplayer Mode: Implement a multiplayer mode to allow players to experience the adventure together.
+  
---
+++ employeeManagement.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Employee
+{
+  public int EmployeeId { get; set; }
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string Department { get; set; }
+  public decimal Salary { get; set; }
+}
+
+class Program
+{
+  static List<Employee> employees = new List<Employee>();
+  static int nextEmployeeId = 1;
+
+  static void Main()
+  {
+    while (true)
+    {
+      Console.WriteLine("\nEmployee Management System");
+      Console.WriteLine("1. Add Employee");
+      Console.WriteLine("2. Edit Employee");
+      Console.WriteLine("3. Display Employees");
+      Console.WriteLine("4. Exit");
+      Console.Write("Enter your choice: ");
+
+      if (int.TryParse(Console.ReadLine(), out int choice))
+      {
+        switch (choice)
+        {
+          case 1:
+            AddEmployee();
+            break;
+          case 2:
+            EditEmployee();
+            break;
+          case 3:
+            DisplayEmployees();
+            break;
+          case 4:
+            Environment.Exit(0);
+            break;
+          default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+        }
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter a number.");
+      }
+    }
+  }
+
+  static void AddEmployee()
+  {
+    Employee employee = new Employee();
+    employee.EmployeeId = nextEmployeeId++;
+    Console.Write("Enter First Name: ");
+    employee.FirstName = Console.ReadLine();
+    Console.Write("Enter Last Name: ");
+    employee.LastName = Console.ReadLine();
+    Console.Write("Enter Department: ");
+    employee.Department = Console.ReadLine();
+    Console.Write("Enter Salary: ");
+    if (decimal.TryParse(Console.ReadLine(), out decimal salary))
+    {
+      employee.Salary = salary;
+      employees.Add(employee);
+      Console.WriteLine("Employee added successfully.");
+    }
+    else
+    {
+      Console.WriteLine("Invalid salary input. Employee not added.");
+    }
+  }
+
+  static void EditEmployee()
+  {
+    Console.Write("Enter Employee ID to edit: ");
+    if (int.TryParse(Console.ReadLine(), out int employeeId))
+    {
+      Employee employee = employees.Find(e => e.EmployeeId == employeeId);
+      if (employee != null)
+      {
+        Console.Write("Enter new Salary: ");
+        if (decimal.TryParse(Console.ReadLine(), out decimal salary))
+        {
+          employee.Salary = salary;
+          Console.WriteLine("Employee details updated successfully.");
+        }
+        else
+        {
+          Console.WriteLine("Invalid salary input. Employee details not updated.");
+        }
+      }
+      else
+      {
+        Console.WriteLine("Employee not found with the given ID.");
+      }
+    }
+    else
+    {
+      Console.WriteLine("Invalid employee ID input.");
+    }
+  }
+
+  static void DisplayEmployees()
+  {
+    Console.WriteLine("\nEmployee Details:");
+    foreach (var employee in employees)
+    {
+      Console.WriteLine($"ID: {employee.EmployeeId}, Name: {employee.FirstName} {employee.LastName}, Department: {employee.Department}, Salary: {employee.Salary:C}");
+    }
+  }
+}
+
